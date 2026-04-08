7a7eb6f7951544bc0eb3baf3649ff71613200831
vboxuser <vboxuser@WIN10>
2026-04-08 10:01:56 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Lukianov_Class.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lukianov_Class
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Лукьянов_йоуEntities context = new Лукьянов_йоуEntities();
+    }
+}
---
+++ Views/Pages/JournalPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lukianov_Class.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для JournalPage.xaml
+    /// </summary>
+    public partial class JournalPage : Page
+    {
+        public JournalPage()
+        {
+            InitializeComponent();
+        }
+
+        private void J_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Report1Page());
+        }
+
+        private void O_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Report2Page());
+        }
+
+        private void Ot_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Report3Page());
+        }
+    }
+}
